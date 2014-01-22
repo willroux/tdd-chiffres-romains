6555e13904f8f8ddd6bb5f86775f2a98a3beab02
william roux <warriorking44@hotmail.fr>
2014-01-22 17:48:52 +0100

    ajout des tab unite et design c plus cool

--- app/models/NombreDecimal.java
+++ app/models/NombreDecimal.java
@@ -13,12 +13,19 @@ public class NombreDecimal {
     }
 
     public String en_romain() {
-    	return en_romain_unites(this.valeur);
+        String unite=en_romain_unites(this.valeur%10);
+        String design=en_romain_design(this.valeur/10);
+    	return (design+unite);
     }
 
-    static String[] table_unites = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    static String[] tab_unites = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    static String[] tab_design = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
 
     public String en_romain_unites(int n) {
-        return table_unites[n];
+        return tab_unites[n];
+    }
+
+    public String en_romain_design(int n) {
+        return tab_design[n];
     }
 }
